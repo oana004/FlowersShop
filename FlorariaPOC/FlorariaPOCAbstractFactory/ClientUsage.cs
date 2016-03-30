e533f9c1378e7e108559896a8a48b8f181ff717c
Oana O <ouatu.oana04@gmail.com>
2016-03-30 21:17:37 +0300

    abstract factory pattern

---
+++ FlorariaPOC/FlorariaPOCAbstractFactory/ClientUsage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlorariaPOCAbstractFactory
+{
+    class ClientUsage
+    {
+        IFlowersAbsFactory flowersFactory; 
+        public void Test()
+        {
+            flowersFactory = new BlackTulipsFactory();
+            //or
+            flowersFactory = new SimpleFlowersFactory();
+
+            var flower = flowersFactory.GetBouquets().Name();
+
+        }
+
+
+        
+    }
+}
---
+++ FlorariaPOC/FlorariaPOCAbstractFactory/FlowersFamily.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlorariaPOCAbstractFactory
+{
+   class RosesBouquet :IBouquets
+    {
+        public string Name()
+        {
+            return "Roses bouqet";
+        }
+    }
+
+   
+    class BlackTulipsBouquet:IBouquets
+    {
+        public string Name()
+        {
+            return "BlackTulipsBouquet";
+        }
+    }
+
+
+    class Roses: ISimpleFlowers
+    {
+        public string Name()
+        {
+            return "Roses";
+        }
+    }
+
+    class Tulips: ISimpleFlowers
+    {
+        public string Name()
+        {
+            return "Tulips";
+        }
+    }
+
+}
